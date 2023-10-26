b352f99dda90ecdaebae8d762b4027695e350884
Anton Onyshchenko <aonyshchenko@censo.co>
2023-10-26 18:21:17 +0200

    Isolate EncryptionKey sharding and recovery to just the owner app

---
+++ Vault/KeyManagement/EncryptionKey+Owner.swift
@@ -0,0 +1,62 @@
+//
+//  EncryptionKey+Owner.swift
+//  Vault
+//
+//  Created by Anton Onyshchenko on 26.10.23.
+//
+
+import Foundation
+import BigInt
+
+extension EncryptionKey {
+    enum ShardingError: Error {
+        case badParticipantId
+    }
+
+    func shard(threshold: Int, participants: [(ParticipantId, Base58EncodedPublicKey)]) throws -> [API.GuardianShard] {
+        let sharer = try SecretSharer(
+            secret: BigInt(privateKeyRaw().toHexString(), radix: 16)!,
+            threshold: threshold,
+            participants: participants.map({$0.0.bigInt})
+        )
+        return try participants.map({ (participantId, participantPublicKey) in
+            guard let shard = sharer.shards.first(where: {$0.x == participantId.bigInt}) else {
+                throw ShardingError.badParticipantId
+            }
+            return API.GuardianShard(
+                participantId: participantId,
+                encryptedShard: try EncryptionKey
+                    .generateFromPublicExternalRepresentation(base58PublicKey: participantPublicKey)
+                    .encrypt(data: shard.y.magnitude.serialize())
+            )
+        })
+    }
+    
+    static func recover(_ encryptedShards: [API.RetrieveRecoveryShardsApiResponse.EncryptedShard], _ session: Session) throws -> EncryptionKey {
+        let points = try encryptedShards.map { encryptedShard in
+            let decryptedShard: Data
+            
+            if encryptedShard.isOwnerShard {
+                guard let ownerApproverKey = encryptedShard.participantId.privateKey(userIdentifier: session.userCredentials.userIdentifier) else {
+                    throw CensoError.failedToRetrieveApproverKey
+                }
+                decryptedShard = try ownerApproverKey.decrypt(base64EncodedString: encryptedShard.encryptedShard)
+            } else {
+                decryptedShard = try session.deviceKey.decrypt(data: encryptedShard.encryptedShard.data)
+            }
+            
+            return Point(
+                x: encryptedShard.participantId.bigInt,
+                y: decryptedShard.toPositiveBigInt()
+            )
+        }
+        
+        return try EncryptionKey.generateFromPrivateKeyRaw(
+            data: SecretSharerUtils.recoverSecret(shares: points).magnitude.serialize().padded(toByteCount: 32)
+        )
+    }
+    
+    static func fromEncryptedPrivateKey(_ base64EncodedString: Base64EncodedString, _ encryptionKey: EncryptionKey) throws -> EncryptionKey {
+        return try EncryptionKey.generateFromPrivateKeyRaw(data: try encryptionKey.decrypt(base64EncodedString: base64EncodedString))
+    }
+}
