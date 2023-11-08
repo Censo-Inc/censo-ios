cb05639357342ebc3c3100d7eaa689c1cf6735b9
Benjamin Holzman <bholzman@censo.co>
2023-11-07 21:58:51 -0500

    Prevent the swiping instructions on the word list from swiping

--- Censo/Views/Censo/SecretsManagement/WordList.swift
+++ Censo/Views/Censo/SecretsManagement/WordList.swift
@@ -11,36 +11,40 @@ struct WordList: View {
     var words: [String]
     
     var body: some View {
-        TabView {
-            ForEach(0..<words.count, id: \.self) { i in
-                VStack {
-                    VStack(spacing: 0) {
-                        Group {
-                            Text(NumberFormatter.ordinal.string(from: NSNumber(value: i + 1)) ?? "") + Text(" word")
-                        }
-                        .font(.title2)
-                        .padding()
-
-                        Text(words[i])
+        VStack {
+            TabView {
+                ForEach(0..<words.count, id: \.self) { i in
+                    VStack {
+                        VStack(spacing: 0) {
+                            Group {
+                                Text(NumberFormatter.ordinal.string(from: NSNumber(value: i + 1)) ?? "") + Text(" word")
+                            }
+                            .font(.title2)
                             .padding()
-                            .multilineTextAlignment(.center)
-                            .font(.title)
-                            .padding([.horizontal])
+                            
+                            Text(words[i])
+                                .padding()
+                                .multilineTextAlignment(.center)
+                                .font(.title)
+                                .padding([.horizontal])
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(lineWidth: 1)
+                        }
+                        .padding([.top, .leading, .trailing], 20)
                     }
-                    .frame(maxWidth: .infinity)
-                    .background {
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(lineWidth: 1)
-                    }
-                    .padding([.top, .leading, .trailing], 20)
-                    Text("swipe back and forth to review words")
-                    Spacer()
-                        .frame(height: 50)
                 }
             }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            Spacer()
+                .frame(height: 20)
+            Text("swipe back and forth to review words")
+            Spacer()
+                .frame(height: 50)
         }
-        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 
