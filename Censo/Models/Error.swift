e68244564aec45134eb7d559e810690d3f433f12
Benjamin Holzman <bholzman@gmail.com>
2023-12-18 13:20:12 -0500

    Revert "Revert "Add validation of import link""
    This reverts commit 91c3d5d8beacb6ddbfd67ebb73d6a2bce8a0fae8.

--- Censo/Models/Error.swift
+++ Censo/Models/Error.swift
@@ -38,6 +38,7 @@ enum CensoError: Swift.Error {
     case productNotFound
     case purchaseFailed
     case failedToGenerateSeedPhrase
+    case linkExpired
     case cannotVerifyMasterKeySignature
 }
 
@@ -104,6 +105,8 @@ extension CensoError: LocalizedError {
             return NSLocalizedString("Product not found", comment: "Error thrown when IAP-product was not found")
         case .failedToGenerateSeedPhrase:
             return NSLocalizedString("Failed to genrate seed phrase", comment: "Failed to genrate seed phrase")
+        case .linkExpired:
+            return NSLocalizedString("This link has expired or is not yet valid", comment: "Expired link")
         case .cannotVerifyMasterKeySignature:
             return NSLocalizedString("Cannot verify master key signature", comment: "Invalid signature")
         }
