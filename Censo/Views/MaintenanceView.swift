089d2afb1bed1649b908f82d904a8a07a74dead2
imykolenko-censo <imykolenko@censo.co>
2024-01-11 16:04:03 +0100

    simplify maintenance timer handling

--- Censo/Views/MaintenanceView.swift
+++ Censo/Views/MaintenanceView.swift
@@ -28,6 +28,12 @@ struct MaintenanceView: View {
                 .edgesIgnoringSafeArea(.all)
                 .background(Color(UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)))
                 .disabled(true)
+                .onAppear {
+                    timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+                }
+                .onDisappear {
+                    timer.upstream.connect().cancel()
+                }
                 .onReceive(timer) { _ in
                     NotificationCenter.default.post(name: Notification.Name.maintenanceStatusCheckNotification, object: nil)
                 }
@@ -35,11 +41,9 @@ struct MaintenanceView: View {
         }
         .opacity(showMaintenanceView ? 1 : 0)
         .onChange(of: scenePhase) { newScenePhase in
-            if newScenePhase == .active && showMaintenanceView {
-                timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
-            } else if newScenePhase == .background {
-                timer.upstream.connect().cancel()
-            }
+           if (newScenePhase == .active && showMaintenanceView) {
+               NotificationCenter.default.post(name: Notification.Name.maintenanceStatusCheckNotification, object: nil)
+           }
         }
         .onChange(of: maintenanceState.isOn) { isMaintenanceMode in
             if !isMaintenanceMode {
