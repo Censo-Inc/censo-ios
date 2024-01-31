0678069fed2814473634c9f7a187a7b2d6d76b07
sburke-censo <56046850+sburke-censo@users.noreply.github.com>
2024-01-31 12:54:21 -0600

    updated test script to handle user input for app and platform testing. (#273)

--- scripts/test_app.sh
+++ scripts/test_app.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
+
 set -eo pipefail
+
+APP="censo"
+TESTPLATFORM="iOS Simulator,OS=17.0,name=iPhone 11"
+#Other TESTPLATFORMS available on the CI IOS Agent
+# TESTPLATFORM="iOS,name=Censo iPhone 11 TXK"
+# TESTPLATFORM="iOS,name=Censo iPhone Xr TXK"
+# TESTPLATFORM="iOS,name=Censo iPhone SE TXK"
+#
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+
+case $key in
+    -t|--testplatform)
+    TESTPLATFORM="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -a|--app)
+    APP="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    shift # past argument
+    ;;
+esac
+
+done
+
+
+
+
 
 if [[ $APP == "approver" ]]; then
   xcodebuild -project "Censo.xcodeproj" \
               -scheme "Approver (Integration)" \
-              -destination platform="iOS Simulator,OS=17.0,name=iPhone 11" \
+              -destination platform="$TESTPLATFORM" \
               clean test | xcpretty
 else
   xcodebuild -project "Censo.xcodeproj" \
               -scheme "Censo (Integration)" \
-              -destination platform="iOS Simulator,OS=17.0,name=iPhone 11" \
+              -destination platform="$TESTPLATFORM" \
               clean test | xcpretty
 fi
