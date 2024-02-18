266223d357d846d2d6bea882ecf76c42569054cb
sburke-censo <56046850+sburke-censo@users.noreply.github.com>
2024-02-17 20:37:27 -0600

    added options for logging and skipping tests (#316)

--- scripts/test_app.sh
+++ scripts/test_app.sh
@@ -1,51 +1,63 @@
 #!/bin/bash
 
-
 set -eo pipefail
 
-APP="censo"
+#Declare variables and set default values
+APP=""
 TESTPLATFORM="iOS Simulator,OS=17.0,name=iPhone 11"
-#Other TESTPLATFORMS available on the CI IOS Agent
-# TESTPLATFORM="iOS,name=Censo iPhone 11 TXK"
-# TESTPLATFORM="iOS,name=Censo iPhone Xr TXK"
-# TESTPLATFORM="iOS,name=Censo iPhone SE TXK"
+#If using a physical phone set TESTPLATFORM="iOS,name=<name of phone>"
 #
+LOGGING=false
+LOGFILE=""
+OUTPUT_OPTS=""
+#
+SKIPTESTS="none"
+#SKIPTESTS="test://com.apple.xcode/Censo/CensoUITests"
+
 
 while [[ $# -gt 0 ]]
 do
-key="$1"
+	key="$1"
 
-
-case $key in
-    -t|--testplatform)
-    TESTPLATFORM="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -a|--app)
-    APP="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    shift # past argument
-    ;;
-esac
-
+	case $key in
+    	-t|--testplatform)
+    	TESTPLATFORM="$2"
+    	shift # past argument
+    	shift # past value
+    	;;
+    	-a|--app)
+    	APP="$2"
+    	shift # past argument
+    	shift # past value
+    	;;
+    	-l|--logging)
+    	LOGGING=true
+    	shift # past argument
+    	;;
+    	*)    # unknown option
+    	shift # past argument
+    	;;
+	esac
 done
 
-
-
+if [ $LOGGING = true ]; then
+	LOGFILE="$APP-output.xml"
+	OUTPUT_OPTS="tee /dev/tty | xcpretty --report junit --output $LOGFILE"
+else
+	OUTPUT_OPTS="xcpretty"
+fi
 
 
 if [[ $APP == "approver" ]]; then
   xcodebuild -project "Censo.xcodeproj" \
               -scheme "Approver (Integration)" \
               -destination platform="$TESTPLATFORM" \
-              clean test | xcpretty
+	      -skip-testing "$SKIPTESTS" \
+              clean test |  eval "$OUTPUT_OPTS"
 else
   xcodebuild -project "Censo.xcodeproj" \
               -scheme "Censo (Integration)" \
               -destination platform="$TESTPLATFORM" \
-              clean test | xcpretty
+	      -skip-testing "$SKIPTESTS" \
+              clean test | eval "$OUTPUT_OPTS"
 fi
